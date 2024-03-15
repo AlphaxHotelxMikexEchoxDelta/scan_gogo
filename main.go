e49c96210bcb2b5e3cb047c163299c31efb39633
AlphaxHotelxMikexEchoxDelta <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2024-03-15 15:45:04 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 	"sync"
 )
 
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorBlue   = "\033[34m"
+	ColorPurple = "\033[35m"
+	colorGrey   = "\033[90m"
+)
+
 type Ports struct {
 	Ports []Data `json:"ports"`
 }
@@ -24,23 +33,23 @@ type Data struct {
 
 func banner(ip string) {
 
-	fmt.Println("     _________")
-	fmt.Println("    / ======= \\")
-	fmt.Println("   / __________\\")
-	fmt.Println("  | ___________ |")
-	fmt.Println("  | | -       | |    @ip_target: " + ip)
-	fmt.Println("  | | scan_go | |    by @BigBurgerBoy")
-	fmt.Println("  | |_________| |")
-	fmt.Println("  \\=____________/")
-	fmt.Println("  / ''''''''''''\\")
-	fmt.Println(" / ::::::::::::: \\")
-	fmt.Println("(_________________)\n")
+	fmt.Println("       _________")
+	fmt.Println("      / ======= \\")
+	fmt.Println("     / __________\\")
+	fmt.Println("    | ___________ |")
+	fmt.Println("    | | -       | |    @ip_target: " + ColorRed + ip + ColorReset)
+	fmt.Println("    | | " + ColorRed + "scan_go" + ColorReset + " | |    by @BigBurgerBoy")
+	fmt.Println("    | |_________| |")
+	fmt.Println("    \\=____________/")
+	fmt.Println("    / ''''''''''''\\")
+	fmt.Println("   / ::::::::::::: \\")
+	fmt.Print("  (_________________)\n")
 
 }
 
-func run(ip string) {
+func scan(ip string) []string {
 
-banner(ip)
+	var opens []string
 
 	content, _ := ioutil.ReadFile("ppt.json")
 	var ports Ports
@@ -48,35 +57,60 @@ banner(ip)
 
 	var wg sync.WaitGroup
 
-	for i := range ports.Ports {
+	for _, port := range ports.Ports {
 
 		wg.Add(1)
 
 		go func(j int) {
 			defer wg.Done()
 
-			conn, err_scan := net.Dial("tcp", ip+":"+strconv.Itoa(ports.Ports[j].Number))
+			conn, err_scan := net.Dial("tcp", ip+":"+strconv.Itoa(port.Number))
 
 			if err_scan == nil {
-				fmt.Println("\n[ "+strconv.Itoa(ports.Ports[j].Number)+" ] "+ports.Ports[j].Name+"\n--> ", ports.Ports[j].Description)
+				opens = append(opens, "   -- "+ColorBlue+strconv.Itoa(port.Number)+ColorReset+":"+ColorReset+ColorBlue+port.Name+ColorReset+" --   "+colorGrey+port.Description+ColorReset+"\n")
 				conn.Close()
 			}
 
-		}(i)
+		}(port.Number)
 	}
 	wg.Wait()
 
+	return opens
+
+}
+
+func run(ip string) map[string][]string {
+
+	var open []string
+	result := make(map[string][]string)
+
+	open = scan(ip)
+
+	if len(open) != 0 {
+		result[ColorReset+"\n  "+ip+"\t\t\t\t["+ColorGreen+"UP"+ColorReset+"]\n"] = open[:]
+	}
+
+	return result
 }
 
 func main() {
 
-	if len(os.Args) != 1 {
+	if len(os.Args[1:]) != 0 {
 
-		run(os.Args[1])
+		banner(os.Args[1])
+
+		resp := run(os.Args[1])
+
+		for ip, ports := range resp {
+			fmt.Print(ip)
+			for port := range ports {
+				fmt.Print(ports[port])
+			}
+		}
 
 	} else {
 
-		fmt.Println("\nSaisi invalide !\n[ main.exe @ip_cible ]\n")
+		fmt.Print(ColorRed + "\nSaisi invalide !" + ColorReset + "\n[ main.exe @ip_cible ]\n\n")
 
 	}
 
